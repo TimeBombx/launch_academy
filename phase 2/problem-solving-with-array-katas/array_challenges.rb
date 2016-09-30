a9b8e6c2f260ebcb142cb2a74cc8b71efea0c9f5
cvotta <cvotta@seczetta.com>
2016-09-30 16:55:33 -0400

    problem solving with array challenge

---
+++ phase 2/problem-solving-with-array-katas/array_challenges.rb
@@ -0,0 +1,12 @@
+# YOUR CODE, HERE
+
+long_string = "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum"
+
+
+secret_message = "7 15 15 4 27 4 15 7"
+
+
+random_numbers = []
+10.times do
+  random_numbers << rand(9) + 1
+end
