6fe15fc533c9d5aae2b5a2f8c80329e434ce1636
cvotta <cvotta@seczetta.com>
2016-10-04 14:31:45 -0400

    Finished the_right_clubs exercise

--- phase 3/the_right_clubs/code.rb
+++ phase 3/the_right_clubs/code.rb
@@ -21,3 +21,14 @@ available_clubs = [
 possible_club_selections = available_clubs.sample(REQUIRED_BAG_SIZE)
 
 # YOUR CODE GOES BELOW
+puts "Automated Golf Club Advice: "
+
+possible_club_selections.each do |selection|
+  puts "Use the #{selection.to_s.gsub("_", " ")}!"
+end
+
+puts
+
+[:driver, :pitching_wedge, :putter].each do |required|
+  puts "You will be without a #{ required.to_s.gsub("_", " ") }" unless possible_club_selections.include?(required)
+end
