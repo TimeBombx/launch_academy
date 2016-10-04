76d0e794413ff3822b2d6818c59a664bc05f5caa
cvotta <cvotta@seczetta.com>
2016-10-04 16:58:09 -0400

    WIP for iteration drills

--- phase 3/iteration-drills/code.rb
+++ phase 3/iteration-drills/code.rb
@@ -1,43 +1,75 @@
 #Write Ruby<->English answers here as comments in your code
 
+# 1. for every element called number in numbers array, print out 3 times that number
+# 2. for every element called name in names array, print the length of the name
+# 3. create a varible called sum and set it equal to zero. for every element, called number, in numbers, add number to sum. print out sum
+# 4. for every key-pair, name and age, print out "name" is "age" years old
+# 5. create a new varible called sum. for every key-pair, transaction and value, add value to sum. print out the sum
+# 6. for every key-pair, name and address, print out "name" is "address" years old
+
 array = [28214, 63061, 49928, 98565, 31769, 42316, 23674, 3540, 34953, 70282, 22077, 94710, 50353, 17107, 73683, 33287, 44575, 83602, 33350, 46583]
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * What is the sum of all the numbers in `array`?
+sum = 0
+array.each { |num| sum += num }
+puts "sum of all nums: #{ sum }"
 # * How would you print out each value of the array?
+puts "Each value of the array: "
+array.each { |value| puts "> #{value}" }
 # * What is the sum of all of the even numbers?
+even = 0
+array.find_all { |e| e % 2 == 0  }.each { |num| even += num }
+puts "sum of all even nums: #{ even }"
 # * What is the sum of all of the odd numbers?
+odd = 0
+array.find_all { |e| e % 1 == 0  }.each { |num| odd += num }
+puts "sum of all even nums: #{ odd }"
 # * What is the sum of all the numbers divisble by 5?
+puts "sum of all the numbers divisble by 5: #{ sum / 5.0 }"
 # * What is the sum of the squares of all the numbers in the array?
+puts "sum of the squares of all the numbers in the array: #{ sum**2 }"
+
 
 array = ["joanie", "annamarie", "muriel", "drew", "reva", "belle", "amari", "aida", "kaylie", "monserrate", "jovan", "elian", "stuart", "maximo", "dennis", "zakary", "louvenia", "lew", "crawford", "caitlyn"]
 
 # Write Ruby code to find out the answers to the following questions:
-
+puts "Each value of the reversed array: "
+array.reverse.each { |value| puts "> #{value}" }
 # * How would you print out each name backwards in `array`?
+puts "Each value of the array backwards: "
+array.each { |value| puts "> #{value.reverse}" }
 # * What are the total number of characters in the names in `array`?
+puts "total number of chars in the array: #{ array.join("").length }"
 # * How many names in `array` are less than 5 characters long?
-# * How many names in `array` end in a vowel?
+puts "Names in the array that are less than 5 chars: #{ array.find_all { |value| value.length < 5 }.count }"
 # * How many names in `array` are more than 5 characters long?
+puts "Names in the array that are more than 5 chars: #{ array.find_all { |value| value.length > 5 }.count }"
 # * How many names in `array` are exactly 5 characters in length?
+puts "Names in the array that are exactly 5 chars: #{ array.find_all { |value| value.length == 5 }.count }"
 
 best_records = {
- "Tupac"=>"All Eyez on Me",
- "Eminem"=>"The Marshall Mathers LP",
- "Wu-Tang Clan"=>"Enter the Wu-Tang (36 Chambers)",
- "Led Zeppelin"=>"Physical Graffiti",
- "Metallica"=>"The Black Album",
- "Pink Floyd"=>"The Dark Side of the Moon",
- "Pearl Jam"=>"Ten",
- "Nirvana"=>"Nevermind"
- }
+  "Tupac"=>"All Eyez on Me",
+  "Eminem"=>"The Marshall Mathers LP",
+  "Wu-Tang Clan"=>"Enter the Wu-Tang (36 Chambers)",
+  "Led Zeppelin"=>"Physical Graffiti",
+  "Metallica"=>"The Black Album",
+  "Pink Floyd"=>"The Dark Side of the Moon",
+  "Pearl Jam"=>"Ten",
+  "Nirvana"=>"Nevermind"
+}
 
 #  Write Ruby code to find out the answers to the following questions:
 
 # * How would you print out all the names of the artists?
+puts "Names of the artists: "
+best_records.each { |key,value| puts "> #{ key }" }
 # * How would you print out all the names of the albums?
+puts "Names of the albums: "
+best_records.each { |key,value| puts "> #{ value }" }
 # * Which artist has the longest name?
+puts "Artist with the longest name: #{ best_records.keys.max { |value| value.length } }"
 # * How would you change all the album titles for every artist to `Greatest Hits`?
 # * How would you delete a key-value pair if the artist's name ends in a vowel?
 
