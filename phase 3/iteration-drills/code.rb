a5c35b1919e9630e72f277d39a6d413f720aabc9
TimeBombx <coreyvotta95@gmail.com>
2016-10-10 11:37:39 -0400

    Finished iteration-drills

--- phase 3/iteration-drills/code.rb
+++ phase 3/iteration-drills/code.rb
@@ -71,130 +71,192 @@ best_records.each { |key,value| puts "> #{ value }" }
 # * Which artist has the longest name?
 puts "Artist with the longest name: #{ best_records.keys.max { |value| value.length } }"
 # * How would you change all the album titles for every artist to `Greatest Hits`?
+puts best_records.each { |key,value| best_records[key] = "Greatest Hits" }
 # * How would you delete a key-value pair if the artist's name ends in a vowel?
+
 
 ages = {"Arch"=>89, "Gretchen"=>93, "Simone"=>12, "Daija"=>96, "Alivia"=>22, "Serena"=>28, "Alek"=>3, "Lula"=>24, "Christian"=>62, "Darryl"=>47, "Otha"=>32, "Evalyn"=>44, "Lincoln"=>27, "Rebeca"=>99, "Beatrice"=>99, "Kelton"=>10, "Zachary"=>18, "Aurelie"=>91, "Dell"=>71, "Lisandro"=>22}
 
 # Write Ruby code to find out the answers to the following questions:
-
+puts
 # * How would you print out all the names of `ages`?
+puts
+puts "Names: "
+ages.each { |key,value| puts key }
 # * How would you print out each key-value pair in the format of `<name> is <age> years old.`?
+puts
+ages.each { |key,value| puts "#{key} is #{value} years old." }
 # * How would you print out every person with odd numbered age?
+puts
+ages.each { |key,value| puts key if value % 1 == 0 }
 # * How would you delete everyone under 25 years of age?
+puts
+ages.delete_if { |key,value| value < 25 }
 # * What is the name and age of everyone with a name greater than or equal to 5 characters?
+puts
+ages.each { |key,value| puts "#{key} is #{value} years old." if key.length >= 5 }
 
 people =
 {
   "Alia O'Conner PhD" => {
-         "phone" => "538.741.1841",
-       "company" => "Leuschke-Stiedemann",
-      "children" => [
-          "Simone",
-          "Cindy",
-          "Jess"
-      ]
+    "phone" => "538.741.1841",
+    "company" => "Leuschke-Stiedemann",
+    "children" => [
+      "Simone",
+      "Cindy",
+      "Jess"
+    ]
   },
-           "Ike Haag" => {
-         "phone" => "(661) 663-8352",
-       "company" => "Carter Inc",
-      "children" => [
-          "Joe",
-          "Ofelia",
-          "Deron"
-      ]
+  "Ike Haag" => {
+    "phone" => "(661) 663-8352",
+    "company" => "Carter Inc",
+    "children" => [
+      "Joe",
+      "Ofelia",
+      "Deron"
+    ]
   },
-       "Brian Heller" => {
-         "phone" => "1-288-601-5886",
-       "company" => "O'Conner Group",
-      "children" => [
-          "Renee"
-      ]
+  "Brian Heller" => {
+    "phone" => "1-288-601-5886",
+    "company" => "O'Conner Group",
+    "children" => [
+      "Renee"
+    ]
   },
-       "Maryse Johns" => {
-         "phone" => "218-571-8774",
-       "company" => "Kuhlman Group",
-      "children" => [
-          "Dominick",
-          "Tricia"
-      ]
+  "Maryse Johns" => {
+    "phone" => "218-571-8774",
+    "company" => "Kuhlman Group",
+    "children" => [
+      "Dominick",
+      "Tricia"
+    ]
   },
   "Dr. Adela DuBuque" => {
-        "phone" => "1-203-483-1226",
-      "company" => "Heidenreich, Nietzsche and Dickinson"
+    "phone" => "1-203-483-1226",
+    "company" => "Heidenreich, Nietzsche and Dickinson"
   }
 }
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * How would you print out all the names of `people`?
+puts
+puts "Names: "
+people.each { |key,value| puts key }
 # * How would you print out all the names of `people` and which company they work for?
+puts
+people.each { |key,value| puts "#{ key } works for #{ people[key]["company"] }" }
 # * What are the names of all the children of everyone in `people`?
+puts
+people.each do |key,value|
+  if people[key].has_key? "children"
+    puts "#{ key } children are: "
+    people[key]["children"].each { |value| puts "> #{ value }" }
+  else
+    puts "#{ key } doesn't have children."
+  end
+end
 # * What are the names of all the companies that people work for?
-# * How would you convert all the phone numbers to the same standard (pick one)?
-
+puts
+people.each { |key,value| puts people[key]["company"] }
 
 people =
 [
-    {
-          "Derek Wehner" => {
-               "phone" => "588-047-7782",
-             "company" => "Daniel-Carroll",
-            "children" => [
-                "Phoebe",
-                "Gretchen",
-                "Wiley"
-            ]
-        },
-           "Ali Koelpin" => {
-               "phone" => "1-127-057-0020",
-             "company" => "Rau, Rutherford and Lockman",
-            "children" => [
-                "Juwan"
-            ]
-        },
-        "Ervin Prohaska" => {
-               "phone" => "(393) 630-3354",
-             "company" => "Carter Inc",
-            "children" => [
-                "Virgil",
-                "Piper",
-                "Josianne"
-            ]
-        },
-          "Hellen Borer" => {
-              "phone" => "1-687-825-0947",
-            "company" => "Maggio, Ferry and Moen"
-        }
+  {
+    "Derek Wehner" => {
+      "phone" => "588-047-7782",
+      "company" => "Daniel-Carroll",
+      "children" => [
+        "Phoebe",
+        "Gretchen",
+        "Wiley"
+      ]
     },
-    {
-        "Elinor Johnson" => {
-              "phone" => "819.911.5553",
-            "company" => "Pollich Group"
-        }
+    "Ali Koelpin" => {
+      "phone" => "1-127-057-0020",
+      "company" => "Rau, Rutherford and Lockman",
+      "children" => [
+        "Juwan"
+      ]
     },
-    {
-        "Richmond Murray" => {
-               "phone" => "1-516-432-2364",
-             "company" => "Sporer and Sons",
-            "children" => [
-                "Kade",
-                "Sage"
-            ]
-        },
-            "Nakia Ferry" => {
-               "phone" => "134-079-2237",
-             "company" => "Hamill, O'Keefe and Lehner",
-            "children" => [
-                "Rollin"
-            ]
-        }
+    "Ervin Prohaska" => {
+      "phone" => "(393) 630-3354",
+      "company" => "Carter Inc",
+      "children" => [
+        "Virgil",
+        "Piper",
+        "Josianne"
+      ]
+    },
+    "Hellen Borer" => {
+      "phone" => "1-687-825-0947",
+      "company" => "Maggio, Ferry and Moen"
     }
+  },
+  {
+    "Elinor Johnson" => {
+      "phone" => "819.911.5553",
+      "company" => "Pollich Group"
+    }
+  },
+  {
+    "Richmond Murray" => {
+      "phone" => "1-516-432-2364",
+      "company" => "Sporer and Sons",
+      "children" => [
+        "Kade",
+        "Sage"
+      ]
+    },
+    "Nakia Ferry" => {
+      "phone" => "134-079-2237",
+      "company" => "Hamill, O'Keefe and Lehner",
+      "children" => [
+        "Rollin"
+      ]
+    }
+  }
 ]
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * What are the names of everyone in `people`?
+puts
+people.each { |value| value.each { |key,value| puts key } }
 # * What are the names of all the children in `people`?
+puts
+people.each { |value| value.each { |key,val| puts val["children"] if val.has_key? "children" } }
+
 # * How would you create a new hash called `phone_numbers` that has a key of a name and value of a phone number in `people`?
+puts
+phone_numbers = Hash.new
+people.each do |value|
+  value.each do |key,val|
+    phone_numbers[key] = val["phone"]
+  end
+end
+
+puts phone_numbers
+
 # * How would you create a new hash called `employers` that has a key of a name and a value of a company name in `people`?
+puts
+employers = Hash.new
+people.each do |value|
+  value.each do |key,val|
+    employers[key] = val["company"]
+  end
+end
+
+puts employers
+
+
 # * How would you create a new hash called `children_count` that has a key of a name and a value of the number of children a person has?
+puts
+children_count = Hash.new
+people.each do |value|
+  value.each do |key,val|
+    children_count[key] = val["children"].count if val.has_key? "children"
+  end
+end
+
+puts children_count
