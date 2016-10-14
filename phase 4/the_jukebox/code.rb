7c20d4f44c5c17150094a63e8f68b17d83505a25
cvotta <cvotta@seczetta.com>
2016-10-14 12:27:00 -0400

    Finished jukebox exercise

---
+++ phase 4/the_jukebox/code.rb
@@ -0,0 +1,41 @@
+class Playlist
+  attr_accessor :playlist
+
+  SONG_LIST = [
+    "Hello - Lionel Ritchie",
+    "Kokomo – The Beach Boys",
+    "Girl You Know It’s True – Milli Vanilli",
+    "Agadoo – Black Lace",
+    "Down Under - Men at Work",
+    "Nothing's Gonna Stop Us Now - Starship",
+    "Get Outta My Dreams, Get Into My Car - Billy Ocean",
+    "I Just Called To Say I Love You - Stevie Wonder",
+    "Hangin' Tough - New Kids on the Block",
+    "We Built This City - Starship",
+    "Wake Me Up Before You Go Go - Wham!",
+    "We Didn't Start The Fire - Billy Joel",
+    "I Wanna Dance With Somebody - Whitney Houston",
+    "U Can't Touch This - MC Hammer"
+  ]
+
+  def initialize( song_list )
+    @playlist
+    song_list.each do |song|
+      if SONG_LIST.include? song
+        @playlist << song
+      end
+    end
+  end
+
+  def shuffle!
+    @playlist.shuffle!
+  end
+
+  def play!
+    @playlist.shift
+  end
+
+  def add_track( song )
+    @playlist << song if SONG_LIST.include? song
+  end
+end
