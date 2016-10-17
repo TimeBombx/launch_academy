065b910cec9c05ea519cef8e1c60cd102eade189
cvotta <cvotta@seczetta.com>
2016-10-17 15:01:14 -0400

    finished movie_sell_out exercise

---
+++ phase 4/movie_sell_out/code.rb
@@ -0,0 +1,24 @@
+class Theater
+  attr_accessor :admitted, :capacity
+
+  def initialize(number_of_seats)
+    @capacity = number_of_seats
+    @admitted = 0
+  end
+
+  def admit!(patrons = 1)
+    if @admitted + patrons > @capacity
+      "Sorry! We're at capacity! Try again later."
+    else
+      @admitted += patrons
+    end
+  end
+
+  def at_capacity?
+    @admitted == @capacity
+  end
+
+  def record_walk_outs!(patrons = 1)
+    @admitted -= patrons
+  end
+end
