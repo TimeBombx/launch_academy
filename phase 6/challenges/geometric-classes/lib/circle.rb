9fe114b65d1e6d11f3bbdf875812643984187976
cvotta <cvotta@seczetta.com>
2016-10-31 11:22:09 -0400

    really finished geometric-classes challenge

--- phase 6/challenges/geometric-classes/lib/circle.rb
+++ phase 6/challenges/geometric-classes/lib/circle.rb
@@ -1,2 +1,20 @@
 class Circle
+  attr_accessor :radius, :x, :y
+
+  def initialize(radius, x = 0, y = 0)
+    @radius = radius
+    @x, @y = x, y
+  end
+
+  def diameter
+    radius * 2
+  end
+
+  def area
+    3.14159 * radius ** 2
+  end
+
+  def perimeter
+    2 * 3.14 * radius
+  end
 end
