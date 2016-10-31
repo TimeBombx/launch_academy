3e82792d1b98b199777c478459886ec525db0f75
cvotta <cvotta@seczetta.com>
2016-10-31 13:21:42 -0400

    finished test-flight challenge

--- phase 6/challenges/test-flight/lib/airplane.rb
+++ phase 6/challenges/test-flight/lib/airplane.rb
@@ -1,2 +1,32 @@
 class Airplane
+  attr_reader :type, :wing_loading, :horse_power
+
+  def initialize(type, wing_loading, horse_power)
+    @type = type
+    @wing_loading = wing_loading
+    @horse_power = horse_power
+    @started = false
+  end
+
+  def start
+    return "airplane already started" if @started
+
+    @started = true
+    "airplane started"
+  end
+
+  def takeoff
+    return "airplane not started, please start" unless @started
+
+    @flying = true
+    "airplane launched"
+  end
+
+  def land
+    return "airplane not started, please start" unless @started
+    return "airplane already on the ground" unless @flying
+
+    @flying = false
+    "airplane landed"
+  end
 end
--- phase 6/challenges/test-flight/spec/lib/airplane_spec.rb
+++ phase 6/challenges/test-flight/spec/lib/airplane_spec.rb
@@ -2,14 +2,65 @@ require_relative "../../lib/airplane"
 
 describe Airplane do
   describe "#initialization" do
+    my_plane = Airplane.new("cesna", 10, 150)
+
+    it 'should have a type' do
+      expect(my_plane.type).to eq("cesna")
+    end
+
+    it 'should have a wing_loading' do
+      expect(my_plane.wing_loading).to eq(10)
+    end
+
+    it 'should have a horse_power' do
+      expect(my_plane.horse_power).to eq(150)
+    end
+
+    it 'type shouldn\'t be editable' do
+      expect{ my_plane.type = "test" }.to raise_error(NoMethodError)
+    end
   end
 
   describe "#start" do
+    my_plane = Airplane.new("cesna", 10, 150)
+
+    it 'should start the plane' do
+      expect(my_plane.start).to eq("airplane started")
+    end
+
+    it 'should already be started' do
+      expect(my_plane.start).to eq("airplane already started")
+    end
   end
 
   describe "#land" do
+    my_plane = Airplane.new("cesna", 10, 150)
+
+    it 'should not be started' do
+      expect(my_plane.land).to eq("airplane not started, please start")
+    end
+
+    it 'should not have taken off' do
+      my_plane.start
+      expect(my_plane.land).to eq("airplane already on the ground")
+    end
+
+    it 'should land on the ground' do
+      my_plane.takeoff
+      expect(my_plane.land).to eq("airplane landed")
+    end
   end
 
   describe "#takeoff" do
+    my_plane = Airplane.new("cesna", 10, 150)
+
+    it 'should not take off' do
+      expect(my_plane.takeoff).to eq("airplane not started, please start")
+    end
+
+    it 'should take off' do
+      my_plane.start
+      expect(my_plane.takeoff).to eq("airplane launched")
+    end
   end
 end
