da154c758f08d1e47515a3dc128e8fb850418497
cvotta <cvotta@seczetta.com>
2016-10-31 11:11:53 -0400

    finished geometric-classes challenge

--- phase 6/challenges/geometric-classes/lib/square.rb
+++ phase 6/challenges/geometric-classes/lib/square.rb
@@ -1,2 +1,28 @@
 class Square
+  attr_accessor :length, :width, :x, :y
+
+  def initialize(side, x = 0, y = 0)
+    @length = side
+    @width = side
+    @x = x
+    @y = y
+  end
+
+  def diameter
+    length * Math.sqrt(2)
+  end
+
+  def perimeter
+    length * 4
+  end
+
+  def area
+    length * width
+  end
+
+  def contains_point?(nx, ny)
+    return false if nx < x || ny < y
+    return false if nx >= (x + length) || ny >= (y + length)
+    return true
+  end
 end
