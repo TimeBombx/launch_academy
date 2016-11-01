9bf13aa6bc51eeb926b25de9db819ba4a2b02cc8
cvotta <cvotta@seczetta.com>
2016-11-01 16:36:07 -0400

    finished testing-blackjack-score exercise

--- phase 6/exercises/testing-blackjack-score/lib/hand.rb
+++ phase 6/exercises/testing-blackjack-score/lib/hand.rb
@@ -8,10 +8,17 @@ class Hand
   def calculate_hand
     # Your code here after writing tests
     # Think about what this method needs to do
+    face = ['J', 'Q', 'K', 'A']
+    score = 0
+    @cards.each do |card|
+      score += 10 if face.any? { |word| card.include?(word) }
+      score += card.to_i
+    end
+    score
   end
 end
 
 deck = Deck.new
 cards = deck.deal(2)
 hand = Hand.new(cards)
-# hand.calculate_hand # Get this working properly
+puts hand.calculate_hand # Get this working properly
--- phase 6/exercises/testing-blackjack-score/spec/lib/hand_spec.rb
+++ phase 6/exercises/testing-blackjack-score/spec/lib/hand_spec.rb
@@ -10,16 +10,9 @@ describe Hand do
   describe "#calculate_hand" do
     # We have included some example tests below. Change these once you get started!
 
-    it "passes" do
-      # Use the RSpec keyword `expect`, as it appears below, to test your assertions
-      expect(1).to eq(1)
-    end
-
-    it "fails" do
-      expect(false).to eq true
-    end
-
     # Add your remaining tests here.
-
+    it 'should calculate correct score' do
+      expect(hand.calculate_hand).to eq(20)
+    end
   end
 end
