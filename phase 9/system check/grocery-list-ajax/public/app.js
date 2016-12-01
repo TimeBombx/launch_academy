507c6a0f619acb56e2d1fc91fe12762c49dd4167
cvotta <cvotta@seczetta.com>
2016-12-01 12:45:52 -0500

    grocery-list-ajax

---
+++ phase 9/system check/grocery-list-ajax/public/app.js
@@ -0,0 +1,41 @@
+$( "#submit" ).submit(function( event ) {
+  event.preventDefault();
+
+  var listItem = $('#grocery_name').val().trim();
+
+  if (listItem.length <= 0)  return;
+
+  var request = $.ajax({
+    method: "POST",
+    data: {
+      'name': listItem
+    },
+    url: ""
+  });
+
+  request.done(function(){
+    // update list
+    $('#grocery-list').append('<li class=\"item\">' + listItem + ' <input type=\"button\" value=\"delete\" class=\"delete\" /></li>');
+    // clear field
+    $('#grocery_name').val("");
+  });
+});
+
+$('.delete').on('click', function( event ){
+  event.preventDefault();
+
+  var parent = $(this).parent();
+  var item = parent.text().trim();
+
+  var request = $.ajax({
+    method: "DELETE",
+    data: {
+      'name': item
+    },
+    url: "/groceries"
+  });
+
+  request.done(function(){
+    parent.remove();
+  });
+});
---
+++ phase 9/system check/grocery-list-ajax/spec/features/03_user_deletes_grocery_item_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+feature "user deletes grocery item", js: true do
+  scenario "successfully deletes grocery item" do
+    visit "/groceries"
+    fill_in "Name", with: "Peanut Butter"
+
+    click_button "Add"
+    click_button "delete"
+    
+    expect(page).to_not have_content "Peanut Butter"
+  end
+end
