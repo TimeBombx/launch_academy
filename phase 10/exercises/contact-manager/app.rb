746e1bc206ca106733d54cc7e916d7a635d0bb75
cvotta <cvotta@seczetta.com>
2016-12-07 15:01:25 -0500

    contact-manager

---
+++ phase 10/exercises/contact-manager/app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find_by_id(params['id'])
+  erb :show
+end
