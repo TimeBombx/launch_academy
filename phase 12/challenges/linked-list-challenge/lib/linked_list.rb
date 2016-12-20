3326af0dfa839a51249ea14b71e339208cab9ce0
cvotta <cvotta@seczetta.com>
2016-12-20 12:34:46 -0500

    linked-list-challenge

---
+++ phase 12/challenges/linked-list-challenge/lib/linked_list.rb
@@ -0,0 +1,84 @@
+require 'pry'
+require_relative 'node'
+
+class LinkedList
+  def initialize()
+    @head = nil
+  end
+
+  def prepend(info)
+    @head = Node.new(info, @head)
+  end
+
+  def [](index)
+    if index <= 0
+      @head
+    else
+      node = @head
+
+      index.times do
+        return nil if node.nil?
+
+        node = node.next_node
+      end
+
+      return node
+    end
+  end
+
+  def to_s
+    array = []
+    node = @head
+
+    while !node.nil?
+      array << node.info
+      node = node.next_node
+    end
+
+    values = array.join(', ') || ""
+
+    return "LinkedList(#{values})"
+  end
+
+  def each
+    node = @head
+
+    while !node.nil?
+      yield(node)
+
+      node = node.next_node
+    end
+  end
+
+  def insert(index, info)
+    if index == 0
+      self.prepend(info)
+    else
+      prev_node = @head
+
+      (index - 1).times do
+        prev_node = prev_node.next_node
+      end
+
+      following_node = prev_node.next_node
+      new_node = Node.new(info, following_node)
+      prev_node.next_node = new_node
+    end
+  end
+
+  def remove(index)
+    if index == 0
+      @head = @head.next_node
+    else
+      prev_node = @head
+
+      (index - 1).times do
+        prev_node = prev_node.next_node
+      end
+
+      node_to_delete = prev_node.next_node
+      following_node = node_to_delete.next_node
+      prev_node.next_node = following_node
+    end
+  end
+end
---
+++ phase 12/challenges/linked-list-challenge/lib/node.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :info, :next_node
+
+  def initialize(info, next_node = nil)
+    @info = info
+    @next_node = next_node
+  end
+end
