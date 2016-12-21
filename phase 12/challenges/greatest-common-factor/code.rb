27049597c2ae047c3008bfcc805a1771ab769b34
cvotta <cvotta@seczetta.com>
2016-12-21 12:03:38 -0500

    greatest-common-factor

---
+++ phase 12/challenges/greatest-common-factor/code.rb
@@ -0,0 +1,8 @@
+#YOUR CODE GOES HERE
+def greatest_common_factor(number1, number2)
+  number1.gcd(number2)
+end
+
+p greatest_common_factor(8, 4)
+p greatest_common_factor(8, 7)
+p greatest_common_factor(81, 18)
